890b3d460d0a405614ec9ed2420db521ddd47f8f
PragyaKimothi <68812506+PragyaKimothi@users.noreply.github.com>
2020-08-11 11:33:06 +0530

    Create q1-a.cpp

---
+++ assignment2/q1-a.cpp
@@ -0,0 +1,27 @@
+paste the code
+#include<iostream>
+using namespace std;
+void rectangle(int l,int b)
+{
+    for(int i=0;i<l;i++)
+    {
+        cout<<"\n";
+        for(int j=0;j<b;j++)
+        {
+            if(i==0||i==l-1||j==0||j==b-1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+    }
+}
+int main()
+{
+    int l,b;
+    cout<<"enter the length : ";
+    cin>>l;
+    cout<<"\nenter the breadth : ";
+    cin>>b;
+    rectangle(l,b);
+    return 0;
+}
